f798f410d2bcd913f45d2eb70d978f8169c39a0e
Josip <d3hum4niz3d@gmail.com>
2018-03-03 22:40:39 +0100

    try to make the script work with new kernel

--- make_defconfig.sh
+++ make_defconfig.sh
@@ -4,14 +4,14 @@ for device in addison; do
 
 DEFCONFIG_FILE=${device}_defconfig
 
-if [ ! -e arch/arm/configs/$DEFCONFIG_FILE ]; then
-	echo "No such file: arch/arm/configs/$DEFCONFIG_FILE"
+if [ ! -e arch/arm64/configs/$DEFCONFIG_FILE ]; then
+	echo "No such file: arch/arm64/configs/$DEFCONFIG_FILE"
 	exit -1
 fi
 
 # make .config
-make ARCH=arm CROSS_COMPILE=arm-eabi- ${DEFCONFIG_FILE} -j8
+make ARCH=arm64 CROSS_COMPILE=clang- ${DEFCONFIG_FILE} -j8
 
-mv .config arch/arm/configs/${DEFCONFIG_FILE}
+mv .config arch/arm64/configs/${DEFCONFIG_FILE}
 done
 make mrproper -j8
